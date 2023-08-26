71987ccc024be9426b3a78974fa178da538f1439
Salman Mujtaba <salmanmujtaba@macbook.home>
2023-08-26 17:12:09 -0400

    Make drop-down selection through code. Determine if error has occured.

--- background.js
+++ background.js
@@ -1,16 +1,16 @@
-let signInCheckBox = document.getElementById('policy_confirmed');
+let isError = false;
+const signInCheckBox = document.getElementById('policy_confirmed');
 if (signInCheckBox) {
   signInCheckBox.click();
 }
 
-// Groups Page
-
+// Groups Page.. continue button
 const cont = document.getElementsByClassName('button primary small')[0];
 if (cont) {
   cont.click();
 }
 
-
+// Rechedule Appointment
 const ul = document.querySelectorAll('.accordion>.accordion-item');
 const resc = ul[3];
 if (resc) {
@@ -18,9 +18,28 @@ if (resc) {
   accordion.children[0].children[1].children[1].children[0].click();
 }
 
+// Appointment drop down. Select location.
+if(window.location.href && window.location.href.includes('appointment')) {
+  let dropDown = document.getElementById('appointments_consulate_appointment_facility_id');
+  if(dropDown) {
+    for(let i=0;i<dropDown.children.length;i++) {
+      if(dropDown.children[i]) {
+        if(dropDown.children[i].textContent.trim().toLocaleLowerCase() === 'calgary') {
+          dropDown.children[i].selected = true;
+        } else {
+          dropDown.children[i].selected = false;
+        }
+      }
+    }
+  }
+  isError = document.getElementById('consulate_date_time_not_available').style.display === "block";
+} 
+
+if(!isError) {
+}
+
 const input = document.getElementById('appointments_consulate_appointment_date');
 const datepicker = document.getElementsByClassName('ui-datepicker-group');
-console.log(datepicker);
 if (input) {
   // simulateClick(input);
   // input.click();
@@ -31,7 +50,7 @@ input.addEventListener('click', () => { console.log(); });
 
 
 const yatri = document.getElementsByClassName('yatri_date')[0];
-console.log(yatri);
+// console.log(yatri);
 // yatri.click();
 
 function simulateClick(input) {
