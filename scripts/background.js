128d31127ea8512e70920300630be3e7a6544cff
Salman Mujtaba <salmanmujtaba@macbook.local>
2023-08-28 01:07:37 -0400

    Add domcontentloaded event

--- scripts/background.js
+++ scripts/background.js
@@ -1,11 +1,26 @@
 let isError = false
 let locations = ['Toronto', 'Ottawa', 'Vancouver', 'Calgary']
 const currentInverviewDate = '2025-09-25'
+document.addEventListener('DOMContentLoaded', () => {
+  clickLoginCheckBox();
+  signIn();
+  clickContinue();
+  recheduleApt();
+  processLocations();
+});
 
-clickLoginCheckBox()
-clickContinue()
-recheduleApt()
-processLocations()
+function signIn() {
+  const dialog = document.getElementsByClassName('ui-dialog infoPopUp ui-widget ui-widget-content ui-front ui-dialog-buttons')[0];
+  console.log(dialog);
+  if(dialog.display !== 'none') {
+    if(dialog.querySelector('.ui-button .ui-corner-all .ui-widget')) {
+      dialog.querySelector('.ui-button .ui-corner-all .ui-widget').click();
+    }
+  }
+  setTimeout(() => {
+    document.querySelector("[type='submit']").click()
+  }, 2000);
+}
 
 function clickLoginCheckBox () {
   const signInCheckBox = document.getElementById('policy_confirmed')
@@ -27,8 +42,8 @@ function recheduleApt () {
   const ul = document.querySelectorAll('.accordion>.accordion-item')
   const resc = ul[3]
   if (resc) {
-    let accordion = resc.children[1]
-    accordion.children[0].children[1].children[1].children[0].click()
+    let accordion = resc.children[1];
+    accordion.children[0].children[1].children[1].children[0].click();
   }
 }
 
@@ -38,19 +53,19 @@ function processLocations () {
     // let form = document.getElementById('appointment-form');
     let dropDown = document.getElementById(
       'appointments_consulate_appointment_facility_id'
-    )
-    let dateTime = document.getElementById('consulate_date_time_not_available')
-    let index = 0
+    );
+    let dateTime = document.getElementById('consulate_date_time_not_available');
+    let index = 0;
     const inter = setInterval(() => {
       if (!isError || index === locations.length) {
-        clearInterval(inter)
-        processDatePickers()
+        clearInterval(inter);
+        processDatePickers();
         return
       } else if (index !== locations.length) {
-        dropDownSelection(locations[index], Array.from(dropDown.children))
+        dropDownSelection(locations[index], Array.from(dropDown.children));
         index++
       }
-      isError = dateTime.style.display !== 'none'
+      isError = dateTime.style.display !== 'none';
     }, 1500)
   }
 }
@@ -62,62 +77,62 @@ function dropDownSelection (itemtoSelect, itemList) {
         itemList[i].textContent.trim().toLocaleLowerCase() ===
         itemtoSelect.trim().toLocaleLowerCase()
       ) {
-        itemList[i].selected = true
+        itemList[i].selected = true;
       } else {
-        itemList[i].selected = false
+        itemList[i].selected = false;
       }
     }
   }
 }
 
 const isDatePickerVisible = () => {
-  const datePicker = document.getElementById('ui-datepicker-div')
-  return datePicker.style.display !== 'none'
+  const datePicker = document.getElementById('ui-datepicker-div');
+  return datePicker.style.display !== 'none';
 }
 
 function processDatePickers () {
   clickDatePicker(() => {
     // When the datepicker is on the screen
     if (isDatePickerVisible()) {
-      clickNextAndFindDate()
+      clickNextAndFindDate();
     }
   })
 }
 
 function clickNextAndFindDate () {
   // Programmatically click the next button
-  const nextButton = document.querySelector('.ui-datepicker-next')
-  nextButton.click()
+  const nextButton = document.querySelector('.ui-datepicker-next');
+  nextButton.click();
 
   // Wait for a brief moment to let the calendar update
   setTimeout(function () {
     // Find the available date from the updated calendar
-    const availableDate = findAvailableDateFromCalendar()
+    const availableDate = findAvailableDateFromCalendar();
 
     if (availableDate) {
-      availableDate.click()
-      selectTime()
+      availableDate.click();
+      selectTime();
     } else {
       // If no available date found, click next and try again
-      clickNextAndFindDate()
+      clickNextAndFindDate();
     }
   }, 500) // Adjust the delay as needed
 }
 
 function findAvailableDateFromCalendar () {
   // Select the calendar table
-  const calendarTable = document.querySelector('.ui-datepicker-calendar')
+  const calendarTable = document.querySelector('.ui-datepicker-calendar');
 
   // Loop through the calendar rows and cells to find an available date
-  const rows = calendarTable.getElementsByTagName('tr')
+  const rows = calendarTable.getElementsByTagName('tr');
   for (let row of rows) {
-    const cells = row.getElementsByTagName('td')
+    const cells = row.getElementsByTagName('td');
     for (let cell of cells) {
       // Check if the cell is selectable (not disabled)
       if (!cell.classList.contains('ui-datepicker-unselectable')) {
         // Extract the date value from the cell content
         // const date = cell.querySelector(".ui-state-default").textContent;
-        return cell
+        return cell;
       }
     }
   }
@@ -126,14 +141,14 @@ function findAvailableDateFromCalendar () {
 }
 
 function clickDatePicker (callback) {
-  console.log('There was no error found so the date can be selected')
+  console.log('There was no error found so the date can be selected');
   const input = document.getElementById(
     'appointments_consulate_appointment_date'
-  ).nextSibling
+  ).nextSibling;
   if (input) {
     setTimeout(() => {
-      input.click()
-      callback()
+      input.click();
+      callback();
     }, 1000)
   }
 }
@@ -143,14 +158,14 @@ function selectTime () {
     // Get the time dropdown element
     const timeDropdown = document.getElementById(
       'appointments_consulate_appointment_time'
-    )
+    );;
 
     // Iterate through options to find a time >= 09:00
     for (let i = 0; i < timeDropdown.options.length; i++) {
-      const option = timeDropdown.options[i]
+      const option = timeDropdown.options[i];
       if (option.value >= '09:00') {
-        option.selected = true
-        processInterviewSelection()
+        option.selected = true;
+        processInterviewSelection();
         break
       }
     }
@@ -158,29 +173,29 @@ function selectTime () {
 }
 
 function processInterviewSelection () {
-  const disabledButton = document.getElementById('appointments_submit')
-  disabledButton.disabled = false
+  const disabledButton = document.getElementById('appointments_submit');
+  disabledButton.disabled = false;
   setTimeout(() => {
-    document.getElementById('appointments_submit_action').click()
-    const revealOverlay = document.querySelector('.reveal-overlay')
+    document.getElementById('appointments_submit_action').click();
+    const revealOverlay = document.querySelector('.reveal-overlay');
     if (revealOverlay && revealOverlay.style.display !== 'none') {
-      const cancelButton = revealOverlay.querySelector('[data-confirm-cancel]')
+      const cancelButton = revealOverlay.querySelector('[data-confirm-cancel]');
       if (cancelButton) {
-        cancelButton.click()
+        cancelButton.click();
       }
     }
   }, 1000)
 }
 
 function compareDates (dateStr1, dateStr2) {
-  const date1 = new Date(dateStr1)
-  const date2 = new Date(dateStr2)
+  const date1 = new Date(dateStr1);
+  const date2 = new Date(dateStr2);
   if (date1 > date2) {
-    return `existing date is later than ${dateStr2}`
+    return `existing date is later than ${dateStr2}`;
     // TODO: rechedule appointment here.
   } else if (date1 < date2) {
-    return `existing date is earlier than ${dateStr2}`
+    return `existing date is earlier than ${dateStr2}`;
   } else {
-    return `existing date is equal to ${dateStr2}`
+    return `existing date is equal to ${dateStr2}`;
   }
 }
