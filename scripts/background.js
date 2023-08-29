467131f91cebf8f5bd2883d6c6a3cc581c917b5b
Salman Mujtaba <salmanmujtaba@macbook.home>
2023-08-29 19:39:43 -0400

    Add conditions to apply logic route wise.

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,26 @@
-let isError = false
+let isError = false;
+let availableConsulateDays = [];
 let locations = ['Toronto', 'Ottawa', 'Vancouver', 'Calgary']
 let currentLocation = "";
-let currentLocationElement = null;
+let currentLocationSelectionElement = null;
 let availableDateMap;
-const currentInverviewDate = '2025-09-25'
-if (window.location.href && window.location.href.includes('appointment')) {
+const isAppointmentPage =
+  window.location.href && window.location.href.includes('appointment');
+const isSignInPage = window.location.href.includes('https://ais.usvisa-info.com/en-ca/niv/users/sign_in');
+const isContinuePage = window.location.href.includes('https://ais.usvisa-info.com/en-ca/niv/groups/36762461');
+const isRechedulePage = window.location.href.includes('https://ais.usvisa-info.com/en-ca/niv/schedule/51484384/continue_actions');
+const currentInverviewDate = '2025-07-29';
+if (isAppointmentPage) {
   class CustomMap extends Map {
     set (key, value) {
       if (!this.has(key)) {
         // Execute your logic when a new value is added
-        console.log(currentLocationElement);
+        console.log(currentLocationSelectionElement);
+        const currentLocation = locations.shift();
+        // if (currentLocation) {
+        //   dropDownSelection(currentLocation, currentLocationSelectionElement);
+        //   setTimeout(() => processDatePickers(), 1000);
+        // }
         console.log(`New value added: Key: ${key}, Value: ${value}`)
       }
       return super.set(key, value)
@@ -18,28 +29,36 @@ if (window.location.href && window.location.href.includes('appointment')) {
   availableDateMap = new CustomMap();
 }
 
-clickLoginCheckBox();
-signIn();
-clickContinue();
-recheduleApt();
-processLocations();
+if (isSignInPage) {
+  clickLoginCheckBox();
+  signIn();
+}
+if (isContinuePage) {
+  clickContinue()
+}
+
+if(isRechedulePage) {
+  recheduleApt();
+}
+
+if(isAppointmentPage) {
+  processLocations()
+}
 
 function signIn() {
-  if(window.location.href.includes('https://ais.usvisa-info.com/en-ca/niv/users/sign_in')) {
-    signInForm();
-    const dialogButton = document.getElementsByClassName('ui-button ui-corner-all ui-widget')[0];
-    if(dialogButton) {
-      dialogButton.click();
-    }
-    setTimeout(() => {
-      document.querySelector('input[type="submit"]').click();
-      console.log('Did I click something??');
-    }, 2000);
+  signInForm();
+  const dialogButton = document.getElementsByClassName('ui-button ui-corner-all ui-widget')[0];
+  if(dialogButton) {
+    dialogButton.click();
   }
+  setTimeout(() => {
+    document.querySelector('input[type="submit"]').click();
+  }, 2000);
 }
 
 function signInForm() {
-
+  document.querySelector('input[type="email"]').value = 'xxx';
+  document.querySelector('input[type="password"]').value = 'xxx';
 }
 
 function clickLoginCheckBox () {
@@ -69,30 +88,34 @@ function recheduleApt () {
 
 // Appointment drop down. Select location.
 function processLocations () {
-  if (window.location.href && window.location.href.includes('appointment')) {
-    // let form = document.getElementById('appointment-form');
-    let dropDown = document.getElementById(
-      'appointments_consulate_appointment_facility_id'
-    );
-    let dateTime = document.getElementById('consulate_date_time_not_available');
-    let index = 0;
-    isError = dateTime.style.display !== 'none'
-    const inter = setInterval(() => {
-      if (!isError || index === locations.length) {
-        clearInterval(inter);
-        processDatePickers();
-        return
-      } else if (index !== locations.length) {
-        dropDownSelection(locations[index], Array.from(dropDown.children));
-        currentLocation = locations[index];
-        index++
-      }
-      isError = dateTime.style.display !== 'none';
-    }, 1500)
-  }
+  // let form = document.getElementById('appointment-form');
+  let dropDown = document.getElementById(
+    'appointments_consulate_appointment_facility_id'
+  );
+  let dateTime = document.getElementById('consulate_date_time_not_available');
+  let index = 0;
+  isError = dateTime.style.display !== 'none';
+  // availableConsulateDays = _this.availableConsulateDays;
+  // console.log('Available consulate days: ', _this.availableConsulateDays);
+  console.log("Do we see any error on the page ", isError);
+  const inter = setInterval(() => {
+    if (!isError || index === locations.length) {
+      clearInterval(inter);
+      processDatePickers();
+      return
+    } else if (index !== locations.length) {
+      dropDownSelection(locations[index], Array.from(dropDown.children));
+      currentLocation = locations[index];
+      index++
+    }
+    isError = dateTime.style.display !== 'none';
+    // console.log('Available consulate days II: ', _this.availableConsulateDays)
+    // availableConsulateDays = _this.availableConsulateDays
+  }, 1500)
 }
 
 function dropDownSelection (itemtoSelect, itemList) {
+  console.log("What location are we trying to select: ", itemtoSelect.trim().toLocaleLowerCase());
   for (let i = 0; i < itemList.length; i++) {
     if (itemList[i]) {
       if (
@@ -100,7 +123,10 @@ function dropDownSelection (itemtoSelect, itemList) {
         itemtoSelect.trim().toLocaleLowerCase()
       ) {
         itemList[i].selected = true;
-        currentLocationElement = itemList[i];
+        itemList[i].dispatchEvent(new Event('change', { bubbles: true }));
+        if (!currentLocationSelectionElement) {
+          currentLocationSelectionElement = itemList[i].parentElement;
+        }
       } else {
         itemList[i].selected = false;
       }
@@ -114,6 +140,7 @@ const isDatePickerVisible = () => {
 }
 
 function processDatePickers () {
+  document.getElementById('appointments_consulate_appointment_date').value = "";
   clickDatePicker(() => {
     // When the datepicker is on the screen
     if (isDatePickerVisible()) {
@@ -143,10 +170,14 @@ function clickNextAndFindDate () {
       availableDate.click();
       const ad = document.getElementById('appointments_consulate_appointment_date').value;
       let isDateAvailable = compareDates(currentInverviewDate, ad);
-      availableDateMap.set(currentLocation, ad);
       console.log(isDateAvailable, 'is date available', ad);
-      if(isDateAvailable) {
+      if (isDateAvailable) {
+        // Current location has a sooner date available
         selectTime();
+      } else {
+        // Current location does not have a sooner date!
+        console.log(`A date was found at `);
+        availableDateMap.set(currentLocation, ad);
       }
     } else {
       // If no available date found, click next and try again
@@ -197,7 +228,7 @@ function selectTime () {
     const timeDropdown = document.getElementById(
       'appointments_consulate_appointment_time'
     );
-    timeDropdown.options[0].selected = true;
+    timeDropdown.options[1].selected = true;
     processInterviewSelection();
   }, 1000)
 }
@@ -233,4 +264,15 @@ function compareDates (dateStr1, dateStr2) {
 const logOut = () => {
   // document.querySelector("a[href='/en-ca/niv/users/sign_out']").click();
 };
+// console.log(chrome, 'webrequest');
+// Check if the chrome supports the webRequest API
+// if (isAppointmentPage && chrome.webRequest && chrome.webRequest.onCompleted) {
+//     // Add an event listener for completed requests
+//   chrome.webRequest.onCompleted.addListener(
+//     details => {
+//       console.log('Request completed:', details.url)
+//     },
+//     { urls: ['<all_urls>'] } // Listen for all URLs
+//   );
+// }
 
